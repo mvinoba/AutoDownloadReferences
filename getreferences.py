a9629345afeb55bc577f1e620434ca45d6d5700c
Vinícius Aguiar Figueiredo <vinoba_@hotmail.com>
2018-05-31 22:51:01 -0300

    Autopep8 file

--- getreferences.py
+++ getreferences.py
@@ -13,8 +13,12 @@ def start_browser():
 
 def get_list_of_references_acmdl(doi):
     driver.get("https://doi.org/" + doi)
-    driver.find_element_by_xpath("//span[.='References']/parent::button").click()
-    refs_table_text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="cf_layoutareareferences"]/div/table/tbody'))).text
+    driver.find_element_by_xpath(
+        "//span[.='References']/parent::button").click()
+    refs_table_text = WebDriverWait(
+        driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="cf_layoutareareferences"]/div/table/tbody'))).text
     refs_list = refs_table_text.split('\n')[1::2]
     return refs_list
 
@@ -24,11 +28,14 @@ def get_references_pdf(refs_list):
     counter = 1
     total = len(refs_list)
     for ref in refs_list:
-        driver.get("https://scholar.google.com.br/scholar?q="+ref)
+        driver.get("https://scholar.google.com.br/scholar?q=" + ref)
         try:
-            pdf_url = driver.find_element_by_class_name("gs_ggs").find_element_by_tag_name('a').get_attribute('href')
-        except:
-            print("{} of {}: Could not find the pdf for: {} \t Skipping...".format(counter, total, ref))
+            pdf_url = driver.find_element_by_class_name(
+                "gs_ggs").find_element_by_tag_name('a').get_attribute('href')
+        except BaseException:
+            print(
+                "{} of {}: Could not find the pdf for: {} \t Skipping...".format(
+                    counter, total, ref))
             counter += 1
             continue
         if not pdf_url.endswith("pdf"):
@@ -44,23 +51,31 @@ def download_all_pdfs(pdfs_url):
     total = len(pdfs_url)
     for url in pdfs_url:
         try:
-            urllib.request.urlretrieve(url, url[url.rindex('/')+1:])
+            urllib.request.urlretrieve(url, url[url.rindex('/') + 1:])
             print("{} of {}: Downloading {} ...".format(counter, total, url))
             counter += 1
         except urllib.error.HTTPError:
-            print("{} of {}: Could not download {}".format(counter, total, url))
+            print(
+                "{} of {}: Could not download {}".format(
+                    counter, total, url))
             counter += 1
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Get all the PDFs in a paper's reference")
-    parser.add_argument("doi", help="tries to find and download all paper's references", type=str)
+    parser = argparse.ArgumentParser(
+        description="Get all the PDFs in a paper's reference")
+    parser.add_argument(
+        "doi",
+        help="tries to find and download all paper's references",
+        type=str)
     args = parser.parse_args()
 
     start_browser()
-    download_all_pdfs(get_references_pdf(get_list_of_references_acmdl(args.doi)))
+    download_all_pdfs(
+        get_references_pdf(
+            get_list_of_references_acmdl(
+                args.doi)))
 
 
 if __name__ == '__main__':
     main()
-
